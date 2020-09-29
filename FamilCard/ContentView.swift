88ea487401b7306af20212ea2c948be22b9b893d
Famil <frostgamingrespawn@gmail.com>
2020-09-30 01:39:15 +0200

    business card app created

--- FamilCard/ContentView.swift
+++ FamilCard/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("famil")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 170, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                
+                Text("Famil Samadli")
+                    .fontWeight(.bold)
+                    .bold()
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .foregroundColor(.white)
+                Text("FullStack Software Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                
+                Divider()
+                InfoView(text: "+36 20 415 86 21", imageName: "phone.fill")
+                InfoView(text: "fsemedli2@gmail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
---
+++ FamilCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  FamilCard
+//
+//  Created by Famil Samadli on 9/30/20.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
